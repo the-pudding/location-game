492de5e368fd50aa07e4b79c7897950ff4c2de66
russellsamora <russellsamora@gmail.com>
2023-03-22 11:42:28 -0400

    done

--- src/components/Index.svelte
+++ src/components/Index.svelte
@@ -1,13 +1,66 @@
 <script>
-	import { getContext } from "svelte";
-	import Demo from "$components/demo/Demo.svelte";
-	import WIP from "$components/helpers/WIP.svelte";
-	// import Footer from "$components/Footer.svelte";
+	import { onMount, getContext } from "svelte";
+	import { base } from "$app/paths";
+	import { shuffle } from "d3";
 
-	// const copy = getContext("copy");
-	// const data = getContext("data");
+	let data = [];
+	let index;
+	let visible;
+
+	onMount(async () => {
+		// load games.json from assets folder
+
+		const response = await fetch(`${base}/assets/games.json`);
+		data = await response.json();
+	});
+
+	function onNewGame() {
+		index = Math.floor(Math.random() * data.length);
+		visible = false;
+	}
+
+	function onReveal() {
+		visible = true;
+	}
+
+	$: game = data[index];
+	$: images = shuffle(game?.images || []).slice(0, 5);
+	$: name = `${game?.name}, ${game?.state_code}`;
+	$: latitude = game?.latitude;
+	$: longitude = game?.longitude;
 </script>
 
-<WIP />
-<Demo />
-<!-- <Footer /> -->
+{#if data}
+	<button on:click={onNewGame}>new game</button>
+	<button on:click={onReveal}>reveal answer</button>
+{/if}
+
+{#if game}
+	<div>
+		<p class="answer" class:visible>
+			{name} ({latitude}, {longitude})
+		</p>
+
+		<ul>
+			{#each images as href, i}
+				<li>
+					<img src={href} alt="random photo {i + 1} of mystery place" />
+				</li>{/each}
+		</ul>
+	</div>
+{/if}
+
+<style>
+	.answer {
+		visibility: hidden;
+	}
+
+	.visible {
+		visibility: visible;
+	}
+
+	li {
+		list-style-type: none;
+		margin: 8px;
+	}
+</style>
